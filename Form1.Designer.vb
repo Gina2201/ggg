c41b5cc06ba64e3146531a6bd3c6820afd90bc72
rodrigh <rodrigh@MININT-H7RNO0Q>
2020-02-01 13:45:15 -0500

    inb

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,9 +22,19 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.SuspendLayout()
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(274, 229)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
 End Class
